939e586695a6a07719f549dc349a275abd757e95
amin1377 <amin1377.mohaghegh@gmail.com>
2023-10-09 10:59:14 -0400

    get header from files

--- result_parse/place_est_accuracy.py
+++ result_parse/place_est_accuracy.py
@@ -13,6 +13,11 @@ def checkHeader(header_line):
 	return header_is_correct
 
 
+def getSortedDict(input_dict):
+	sorted_dict = {k: input_dict[k] for k in sorted(input_dict)}
+	return sorted_dict
+
+
 
 
 def getNetValPair(file_lines):
@@ -21,6 +26,7 @@ def getNetValPair(file_lines):
 	net_val_pair = {}
 	for line_num, line in enumerate(file_lines):
 		if line_num == 0:
+			header = line
 			assert checkHeader(line)
 		else:
 			split_line = line.split()
@@ -29,7 +35,7 @@ def getNetValPair(file_lines):
 			val = None
 			net_id = int(split_line[0])
 			if len(split_line) == 2:
-				val = split_line[1]
+				val = flaot(split_line[1])
 			else:
 				assert len(split_line) == 3
 				sink_id = int(split_line[1])
@@ -44,7 +50,7 @@ def getNetValPair(file_lines):
 				else:
 					net_val_pair[net_id] = val
 
-	return net_val_pair
+	return header, net_val_pair
 
 
 
@@ -61,14 +67,29 @@ def main(act_file_name, est_file_name, out_file_name):
 	with open(est_file_name, 'r') as est_file:
 		est_file_lines = [line.strip() for line in est_file.readlines()]
 
-	act_file_net_val_pair = getNetValPair(act_file_lines)
-	act_file_net_val_pair = {k: act_file_net_val_pair[k] for k in sorted(act_file_net_val_pair)}
+	header_line, act_file_net_val_pair = getNetValPair(act_file_lines)
+	act_file_net_val_pair = getSortedDict(act_file_net_val_pair)
 
-	est_file_net_val_pair = getNetValPair(est_file_lines)
-	est_file_net_val_pair = {k: est_file_net_val_pair[k] for k in sorted(est_file_net_val_pair)}
+	_, est_file_net_val_pair = getNetValPair(est_file_lines)
+	est_file_net_val_pair = getSortedDict(est_file_net_val_pair)
 
 	with open(out_file_name, 'w') as out_file:
 		assert len(act_file_net_val_pair) == len(est_file_net_val_pair)
+		for net_id in act_file_net_val_pair:
+			assert net_id in est_file_net_val_pair
+			act_val = act_file_net_val_pair[net_id]
+			est_val = est_file_net_val_pair[net_id]
+			
+			assert len(act_val) == len(est_val)
+			assert isinstance(act_val, dict) == isinstance(est_val, dict) 
+
+			if isinstance(act_val, dict):
+				act_val = getSortedDict(act_val)
+				est_val = getSortedDict(est_val)
+			else:
+				ratio = est_val / est_val
+				out_file_name.write(f"")
+
 
 
 
