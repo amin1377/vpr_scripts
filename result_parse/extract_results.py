49d73ec136fd124c610cc9921424c3731286ea9b
amin1377 <amin1377.mohaghegh@gmail.com>
2024-11-14 15:27:41 -0500

    parse pack/place/route/total time

--- result_parse/extract_results.py
+++ result_parse/extract_results.py
@@ -8,8 +8,16 @@ def extract_circuit_info(contents):
     
     cpd = -1
     wl = -1
+    pack_time = -1
+    place_time = -1
+    route_time = -1
+    total_time = -1
 
     for line in lines:
+        pack_time_line = re.search(r"# Packing took ([\d.]+) seconds", line)
+        place_time_line = re.search(r"# Placement took ([\d.]+) seconds", line)
+        route_time_line = re.search(r"# Routing took ([\d.]+) seconds", line)
+        total_time_line = re.search(r"The entire flow of VPR took ([\d.]+) seconds", line)
         if "Final critical path delay" in line:
             pattern = r'Final critical path delay \(least slack\)\s*:\s*(.*) ns'
             match = re.search(pattern, line)
@@ -24,18 +32,30 @@ def extract_circuit_info(contents):
             if match:
                 number = float(match.group(1))
                 wl = number
+        elif pack_time_line:
+            pack_time = float(pack_time_line.group(1))
+        elif place_time_line:
+            place_time = float(place_time_line.group(1))
+        elif route_time_line:
+            route_time = float(route_time_line.group(1))
+        elif total_time_line:
+            total_time = float(total_time_line.group(1))
 
     assert cpd > 0
     assert wl > 0
+    assert pack_time > 0
+    assert place_time > 0
+    assert route_time > 0
+    assert total_time > 0
 
-    return cpd, wl
+    return cpd, wl, pack_time, place_time, route_time, total_time
 
 
 def check_vpr_output(task_dir, out_file_name):
     circuits = os.listdir(task_dir)
     print(f"Circuits: {circuits}\n\n")
 
-    data = [["Circuit", "Td", "WL"]]
+    data = [["Circuit", "Td", "WL", "Pack", "Place", "Route", "Total"]]
     for circuit in circuits:
         circuit_dir = os.path.join(task_dir, circuit, "common")
         vpr_out_file = os.path.join(circuit_dir, "vpr.out")
@@ -49,14 +69,14 @@ def check_vpr_output(task_dir, out_file_name):
                 if not "VPR succeeded" in vpr_out_content:
                     print(f"{circuit_name} failed!")
                     continue
-                cpd, wl = extract_circuit_info(vpr_out_content)
-                data.append([f"{circuit_name}", f"{cpd:.2f}", f"{wl}"])
+                cpd, wl, pa_time, pl_time, r_time, t_time = extract_circuit_info(vpr_out_content)
+                data.append([f"{circuit_name}", f"{cpd:.2f}", f"{wl}", f"{pa_time}", f"{pl_time}", f"{r_time}", f"{t_time}"])
 
         else:
             print(f"Couldn't find {vpr_out_file}")
 
     with open(out_file_name, "w") as csv_file:
-        csv_writer = csv.writer(csv_file)
+        csv_writer = csv.writer(csv_file, delimiter="\t")
         csv_writer.writerows(data)
 
 
